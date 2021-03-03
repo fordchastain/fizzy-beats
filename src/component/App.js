ae107067c551cff9f59f90305c665511b8ee4b09
unknown <fordchastain@yahoo.com>
2021-03-02 23:11:59 -0600

    Added NumStepper component

--- src/component/App.js
+++ src/component/App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import ButtonPanel from "./ButtonPanel"
+import NumStepper from "./NumStepper"
 import "./App.css";
 
 export default class App extends React.Component {
+
+  handleInput = (value) => {
+    console.log(value);
+  }
+
   render() {
     return (
       <div className="component-app">
-      <ButtonPanel />
+        <div className="content-wrapper">
+          <NumStepper inputHandler={this.handleInput} />
+          <ButtonPanel />
+        </div>
       </div>
     );
   }
---
+++ src/component/NumStepper.js
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./NumStepper.css";
+
+export default class Button extends React.Component {
+  static propTypes = {
+    min: PropTypes.number,
+    max: PropTypes.number,
+    inputHandler: PropTypes.func,
+  };
+
+  state = {value: 120};
+
+  handleInput = (event) => {
+    this.props.inputHandler(event.target.value);
+    this.setState({value: event.target.value});
+  };
+
+  render() {
+    return (
+      <div className="component-num-stepper">
+        <label for="bpm">BPM:</label>
+        <input type="number" id="bpm" name="bpm" min={this.props.min} 
+          max={this.props.max} value={this.state.value} onInput={this.handleInput}/>
+      </div>
+    );
+  }
+}
